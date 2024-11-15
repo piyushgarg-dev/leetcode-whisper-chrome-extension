808a8795473ad81631d6986b9784c8e7f7c78258
WebDevChandan <hirechandan@gmail.com>
2024-11-16 03:11:37 +0530

    Add OpenAI API Validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
 import leetCode from '@/assets/leetcode.png';
-import { Input } from './components/ui/input';
+import React from 'react';
+import { z } from "zod";
 import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
 
+const openAIApiKeySchema = z.string().superRefine((key, ctx) => {
+  // Check the minimum length to 32
+  if (key.length < 32) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.too_small,
+      minimum: 32,
+      type: "string",
+      inclusive: true,
+      message: "Invalid API key",
+    });
+    return;
+  }
+
+  // If OpenAI API dones't start with sk-anyCombination_of_alphanumeric
+  if (!/^sk-[a-zA-Z0-9-_]+$/.test(key)) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.invalid_string,
+      validation: "regex",
+      message: "Invalid API key",
+    });
+  }
+});
 const Popup: React.FC = () => {
   const [openAIKey, setOpenAIKey] = React.useState('');
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const [isInvalidApiKey, setIsInvalidApiKey] = React.useState('');
 
   React.useEffect(() => {
     (async function loadOpenAPIKey() {
@@ -18,9 +42,25 @@ const Popup: React.FC = () => {
     })();
   }, []);
 
+  React.useEffect(() => {
+    if (!openAIKey)
+      setIsInvalidApiKey(""); //Clear Error Message, if API key removed
+
+  }, [openAIKey])
+
   const handleAddOpenAPIKey = async () => {
     if (openAIKey) {
-      await chrome.storage.local.set({ apiKey: openAIKey });
+      try {
+        const validKey = openAIApiKeySchema.parse(openAIKey);
+        setIsInvalidApiKey("");
+        await chrome.storage.local.set({ apiKey: validKey });
+
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          const errorMessage = error.issues.map(issue => issue.message).join(", ");
+          setIsInvalidApiKey(errorMessage);
+        }
+      }
     }
   };
 
@@ -36,13 +76,15 @@ const Popup: React.FC = () => {
           </div>
           <div className="mt-10 flex flex-col gap-2">
             <label htmlFor="text" className='text-white font-bold text-xl'>Enter Your OpenAI API key</label>
+            <label htmlFor="text" className='text-red-400 text-base'>{isInvalidApiKey}</label>
             <Input
               value={openAIKey}
               onChange={(e) => setOpenAIKey(e.target.value)}
               placeholder="Ex. 0aBbnGgzXXXXXX"
               className='bg-white outline-none text-black'  //API input box Text color turned white to Black
+              onKeyDown={(e) => e.key === 'Enter' ? handleAddOpenAPIKey() : null}   //API Key Validation on pressing "Enter" key
             />
-            <Button onClick={handleAddOpenAPIKey} className="dark">
+            <Button onClick={handleAddOpenAPIKey} className={openAIKey ? `dark` : `bg-gray-400 cursor-not-allowed hover:bg-gray-400`}>
               Save
             </Button>
           </div>
