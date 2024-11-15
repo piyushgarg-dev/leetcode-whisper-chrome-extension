3481a96e16049a8149ed39e2bfc8bb63d040611f
ayat-delineate <ayat@delineate.pro>
2024-11-16 02:42:11 +0600

    style(content): ui style fixed

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -40,7 +40,13 @@ interface ChatMessage {
 
 function ChatBox({ context }: ChatBoxProps) {
   const [value, setValue] = React.useState("");
-  const [chatHistory, setChatHistory] = React.useState<ChatMessage[]>([]);
+  const [chatHistory, setChatHistory] = React.useState<ChatMessage[]>([
+    {
+      role: "assistant",
+      message: "Hello",
+      type: "markdown",
+    },
+  ]);
 
   const chatBoxRef = useRef<HTMLDivElement>(null);
 
@@ -108,8 +114,11 @@ function ChatBox({ context }: ChatBoxProps) {
           return [...prev];
         });
 
-        // scroll to the bottom of the chat box
-        chatBoxRef.current?.scrollIntoView({ behavior: "smooth" });
+        // scroll to the bottom of the
+        chatBoxRef.current?.scrollTo({
+          top: chatBoxRef.current.scrollHeight,
+          behavior: "smooth",
+        });
       } catch (e) {
         console.error("Error parsing JSON", e);
       }
@@ -126,15 +135,21 @@ function ChatBox({ context }: ChatBoxProps) {
     handleGenerateAIResponse();
   };
   return (
-    <div className="w-[400px] h-[550px] mb-2 rounded-xl relative text-wrap overflow-auto">
-      <div className="h-[510px] overflow-auto" ref={chatBoxRef}>
+    <div className="w-[400px] h-[550px] mb-2 rounded-sm relative text-wrap overflow-auto bg-white shadow-sm border">
+      <div
+        className="h-[510px] overflow-auto bg-white shadow-sm"
+        style={{
+          zIndex: 1000,
+        }}
+        ref={chatBoxRef}
+      >
         {chatHistory.map((message, index) => (
           <div
             key={index.toString()}
-            className="flex gap-4 mt-3 w-[400px] text-wrap"
+            className="flex gap-4 mt-3 w-[400px] text-wrap px-2"
           >
             <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarImage src="https://github.com/shadcn.png" loading="lazy" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <div className="w-[100%]">
@@ -149,14 +164,14 @@ function ChatBox({ context }: ChatBoxProps) {
         ))}
       </div>
 
-      <div className="absolute bottom-0 w-full flex items-center gap-2">
+      <div className="absolute bottom-0 w-full flex items-center gap-2 bg-white p-2">
         <Input
           value={value}
           onChange={(e) => setValue(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") onSendMessage();
           }}
-          className="rounded-lg bg-black"
+          className="rounded-lg bg-white text-black"
           placeholder="Type your message here"
         />
         <SendHorizontal onClick={onSendMessage} className="cursor-pointer" />
@@ -173,7 +188,7 @@ const ContentPage: React.FC = () => {
   const problemStatement = metaDescriptionEl?.getAttribute("content") as string;
 
   return (
-    <div className="__chat-container dark">
+    <div className="__chat-container dark z-[100]">
       {chatboxExpanded && (
         <ChatBox context={{ problemStatement, programmingLanguage: "C++" }} />
       )}
