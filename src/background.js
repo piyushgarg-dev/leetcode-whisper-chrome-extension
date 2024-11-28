b16afd8687ae04ec216ce6ffaebe3d94cf741b73
Akash kumar kumawat <kumawatap63@gmail.com>
2024-11-28 19:20:39 +0530

    feat: Auto Reload leetcode page on extension install

--- src/background.js
+++ src/background.js
@@ -3,3 +3,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.action.openPopup() // Opens the popup programmatically
   }
 })
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install' || details.reason === 'update') {
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        if (tab.url && /^https:\/\/leetcode\.com(\/.*)?$/.test(tab.url)) {
+          chrome.tabs.reload(tab.id, () => {})
+        }
+      })
+    })
+  }
+})
